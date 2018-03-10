c488c5ad0e0121c3647502823b48af276b47107f
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-10 21:55:03 +0530

    Create OddInterval.java

---
+++ OddInterval.java
@@ -0,0 +1,20 @@
+
+
+public class OddInterval {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+    System.out.println("enter the first interval:");
+        int num1=scan.nextInt();
+    System.out.println("enter the second interval:");
+       int num2=scan.nextInt();  
+       while(num1<=num2) {
+    	   if(num1%2!=0) {
+			   System.out.println(num1);
+    	   }
+    	   num1=num1+1;
+    	}
+        scan.close();
+       }
+    }
+
+

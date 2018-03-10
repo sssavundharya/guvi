91ed681a5ef232f9fced40383cadddeacab1334c
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-10 13:56:43 +0530

    Create HelloString.java

---
+++ HelloString.java
@@ -0,0 +1,15 @@
+
+
+public class HelloString {
+	public static void main(String [] args) {
+		  Scanner scan = new Scanner(System.in);
+	  	  System.out.println(" enter n:");
+          int n=scan.nextInt();
+          for(int i=0;i<n;i++) {
+     		 System.out.println(" hello");
+
+        	  
+          }
+	}
+     
+}

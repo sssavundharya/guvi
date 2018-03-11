8200dc5db3d0a5ab9026c46c2cd2023a8a5a5cde
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-12 05:08:11 +0530

    Create cmp.py

---
+++ cmp.py
@@ -0,0 +1,11 @@
+def compares(str1,str2):
+    if str1==str2:
+        print(str2)
+    elif str1>str2:
+        print(str1)
+    else:
+        print(str2)
+s1=input()
+s2=input()
+compares(s1,s2)
+    

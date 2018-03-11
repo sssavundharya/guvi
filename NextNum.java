06c5960bee0a10164cb70b7a188cbd946a3c5880
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-12 05:24:55 +0530

    Create NextNum.java

---
+++ NextNum.java
@@ -0,0 +1,15 @@
+
+
+public class NextNum  {
+	public static void main(String [] args) {
+		 
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("enter the number:");
+		 int n=scan.nextInt();
+		 n+=1;
+		 System.out.println(n);
+
+		 
+	}
+
+}

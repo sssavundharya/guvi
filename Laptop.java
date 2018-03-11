98ffaa278d2e76351c60695764f75f30171e5dbd
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-12 05:05:45 +0530

    Create Laptop.java

---
+++ Laptop.java
@@ -0,0 +1,17 @@
+
+
+public class Laptop {
+	public static void main(String [] args) {
+		  Scanner scan = new Scanner(System.in);
+	  	  System.out.println(" enter n:");
+          int n=scan.nextInt();
+	  	  System.out.println(" enter string:");
+	  	  String s= scan.next();
+          for(int i=0;i<n;i++) {
+     		 System.out.println(s);
+
+        	  
+          }
+	}
+     
+}

8f80c84edf3457395a8cc5452b84493a02177d84
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-12 20:14:10 +0530

    Create FibMod6.java

---
+++ FibMod6.java
@@ -0,0 +1,21 @@
+
+
+public class FibMod6 {
+	 public static void main(String args[]){ 
+		   int a=0,b=1,c=1;
+		   Scanner Scan=new Scanner(System.in);
+		  System.out.println("enter the number:");
+		  int num=Scan.nextInt();
+		  for(int i=0;i<num;i++) {
+			  a=b;
+			  b=c;
+			  c=a+b;
+			 System.out.println(a+"");
+			  
+        Scan.close();
+
+
+		  
+	 }
+
+	 }}

6d30b6c94ed0868207cda07d1a8b936c0a62ff4a
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-18 23:51:09 +0530

    Create modu109.py

---
+++ modu109.py
@@ -0,0 +1,11 @@
+#109
+def min(n):
+    a=[]
+    for i in rangr(n):
+        a.append(int(input()))
+        mini=a[0]
+        for i in a:
+            if i<mini:
+                min=i
+    print(mini)
+minimum(10)

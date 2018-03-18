30a7fd2e77ad2b37e5d926782807804c47783e3d
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2018-03-18 16:37:34 +0530

    Create finding.py

---
+++ finding.py
@@ -0,0 +1,11 @@
+
+num=int(input())
+b=[]
+for i in range(num+1):
+    a.append(int(input()))
+    l=list(b)
+for i in range(1,num+1):
+    if(l[i-1]>l[i]):
+        print(l[i-1])
+        break
+    

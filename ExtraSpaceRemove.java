fefb1292118cc21fad13144a99e825a66f37ee7a
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2019-05-02 14:46:17 +0530

    Create ExtraSpaceRemove.java

---
+++ ExtraSpaceRemove.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scan=new Scanner(System.in);
+       String s=scan.nextLine();
+       System.out.println(s.replaceAll("  ",""));
+    }
+}

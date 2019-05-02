07cba9bc2166fca5d2e8e3b8bd241f2d56b454d7
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2019-05-02 11:39:35 +0530

    Create MaxChar1.java

---
+++ MaxChar1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
+       //System.out.print(str.length());
+      
+        char[]a=str.toCharArray();
+        int count=0,max=0;
+        String s="";
+        for(int i=0;i<a.length;i++){
+            count=0;
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j])count++;
+                
+            }
+            if(count>=max){
+                max=count;
+                s=a[i]+"";
+            }
+        }
+        System.out.println(s);
+    }
+}

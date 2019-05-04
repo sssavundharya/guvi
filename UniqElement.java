613aa4f337be1afae7062aee7634a489da26319a
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2019-05-04 11:05:31 +0530

    Create UniqElement.java

---
+++ UniqElement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            count=0;
+            for(int j=0;j<n;j++){
+                if(a[i]==a[j] && i!=j){
+                    count=1;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
